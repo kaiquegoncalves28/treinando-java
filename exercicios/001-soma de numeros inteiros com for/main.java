b5479d790bdc0a0e95a0520eace3faa3d428f08d
kaiquegoncalves28 <kaiquesilvags728@gmail.com>
2026-07-15 19:31:59 -0300

    novo exercício, soma dos numeros de 1 a 10

---
+++ exercicios/001-soma de numeros inteiros com for/main.java
@@ -0,0 +1,12 @@
+public class main {
+	public static void main(String[] args) {
+
+		// programa que soma os números de 1 a 10
+		int soma = 0;
+
+		for (int numero = 1; numero <= 10; numero++) {
+			soma+=numero;
+		}
+	System.out.println("a soma dos números de 1 a 10 é " + soma);
+	}
+}
