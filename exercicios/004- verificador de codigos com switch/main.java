572a09a212ebe97efee0e81341c64ef5d466c826
kaiquegoncalves28 <kaiquesilvags728@gmail.com>
2026-07-16 13:15:43 -0300

    ex 004

---
+++ exercicios/004- verificador de codigos com switch/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        /* programa java que verifica códigos validos de uma carteirinha de hospital */
+        
+        int numeroCarteirinha = 22;
+
+             switch (numeroCarteirinha) {
+            case 11:
+            case 22:
+            case 33:
+            case 44:
+            case 55:
+                System.out.println("Carteirinha aceita pelo Hospital, o número da Carteirinha é = " + numeroCarteirinha);
+                break;
+            default:
+                System.out.println("Carteirinha não aceita pelo Hospital, número da Carteirinha é = " + numeroCarteirinha);
+                break;
+
+        }
+    }
+}
