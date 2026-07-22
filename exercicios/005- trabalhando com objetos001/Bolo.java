1e5103bdc5492fd3a84f36aa82ef1461fa123386
kaiquegoncalves28 <kaiquesilvags728@gmail.com>
2026-07-22 09:33:05 -0300

    ex 005

---
+++ exercicios/005- trabalhando com objetos001/Bolo.java
@@ -0,0 +1,28 @@
+public class Bolo {
+	
+	int quantidadeAcucar = 288;
+	//colocando um valor nessa 
+
+	public static void main(String[] args) {
+
+
+	Bolo boloDeChocolate = new Bolo();
+
+	//boloDeChocolate.quantidadeAcucar = 200;
+
+
+	Bolo boloDeBaunilha = new Bolo();
+
+	//boloDeBaunilha.quantidadeAcucar = 150;	
+
+
+	System.out.println("Quantidade de açucar no bolo de chocolate: " 
+		+ boloDeChocolate.quantidadeAcucar + " gramas");
+
+	System.out.println("Quantidade de açucar no bolo de baunilha: " 
+		+ boloDeBaunilha.quantidadeAcucar + " gramas");
+
+
+
+	}
+}
