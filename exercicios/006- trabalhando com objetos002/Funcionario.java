792b8d0ab9b721bdc8cb124eb3e5886224c2bfca
kaiquegoncalves28 <kaiquesilvags728@gmail.com>
2026-07-22 11:16:03 -0300

    ex 006 atualizado

--- exercicios/006- trabalhando com objetos002/Funcionario.java
+++ exercicios/006- trabalhando com objetos002/Funcionario.java
@@ -1,25 +1,43 @@
 public class Funcionario {
-
 	String nome;
 	String cargo;
 	int idade;
-
 	public static void main(String[] args){
 
-		Funcionario funcionario = new Funcionario();
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.nome = "Kaíque";
+		funcionario1.cargo = "Desenvolvedor junior";
+		funcionario1.idade = 19;
 
-		funcionario.nome = "Kaíque";
-		funcionario.cargo = "Desenvolvedor";
-		funcionario.idade = 19;
+		Funcionario funcionario2 = new Funcionario();
+		funcionario2.nome = "Marcelo Oliveira";
+		funcionario2.cargo = "Gestor de vendas";
+		funcionario2.idade = 35;
 
+		Funcionario funcionario3 = new Funcionario();
+		funcionario3.nome = "Mauro";
+		funcionario3.cargo = "Desenvolvedor senior";
+		funcionario3.idade = 28;
+		
 
-		System.out.println("nome: " + funcionario.nome);
+		System.out.println("FUNCIONARIOS DA EMPRESA");
 
-		System.out.println("cargo: " + funcionario.cargo);
+		System.out.println("nome: " + funcionario1.nome);
+		System.out.println("cargo: " + funcionario1.cargo);
+		System.out.println("idade: " + funcionario1.idade);
 
-		System.out.println("idade: " + funcionario.idade);
+		System.out.println("---------------------------");
+
+		System.out.println("nome: " + funcionario2.nome);
+		System.out.println("cargo: " + funcionario2.cargo);
+		System.out.println("idade: " + funcionario2.idade);
+
+		System.out.println("---------------------------");
+
+		System.out.println("nome: " + funcionario3.nome);
+		System.out.println("cargo: " + funcionario3.cargo);
+		System.out.println("idade: " + funcionario3.idade);
 
 
 	}
-
 }
