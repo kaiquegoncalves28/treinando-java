0cea19f07e53ef47f95912c32c57546043d21dbb
Kaique <kaiquesilvags728@gmail.com>
2026-07-29 09:38:11 -0300

    ex 007

---
+++ exercicios/007- usando construtores/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+	float saldo;
+	String nome;
+	String cargo;
+	
+	Main(float	saldo, String nome, String cargo) {
+		this.saldo = saldo;
+		this.nome = nome;
+		this.cargo = cargo; 
+	}
+	
+	public static void main(String[] args) {
+
+		Main pessoa2 = new Main(2008,"lila","dev junior");
+
+		Main pessoa = new Main(3000,"kaique","dev");
+
+		System.out.println(pessoa.nome);
+		System.out.println(pessoa.cargo);
+		System.out.println(pessoa.saldo);
+		System.out.println("--------------------");
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.cargo);
+		System.out.println(pessoa2.saldo);
+	}
+}
