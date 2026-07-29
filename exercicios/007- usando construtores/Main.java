81af66f50f764392f0e432a18c378a55756b58c6
Kaique <kaiquesilvags728@gmail.com>
2026-07-29 11:41:00 -0300

    ex007 atualizado

--- exercicios/007- usando construtores/Main.java
+++ exercicios/007- usando construtores/Main.java
@@ -7,25 +7,37 @@ public class Main {
 		nome = nomeInit;
 		cargo = cargoInit;
 	}
-	
-	/*Main(float	saldo, String nome, String cargo) {
-		this.saldo = saldo;
-		this.nome = nome;
-		this.cargo = cargo; 
-	}*/
+	//construtor vazio para usar na pessoa 3
+	Main() {
+	}
 	
 	public static void main(String[] args) {
 
-		Main pessoa2 = new Main(2008,"lila","dev junior");
-
 		Main pessoa = new Main(3000,"kaique","dev");
+
+		Main pessoa2 = new Main(2008,"lila","dev junior");
 
 		System.out.println(pessoa.nome);
 		System.out.println(pessoa.cargo);
 		System.out.println(pessoa.saldo);
+
 		System.out.println("--------------------");
+
 		System.out.println(pessoa2.nome);
 		System.out.println(pessoa2.cargo);
 		System.out.println(pessoa2.saldo);
+
+		System.out.println("--------------------");
+
+		Main pessoa3 = new Main();
+		pessoa3.saldo = 3899;
+		pessoa3.nome = "Amanda";
+		pessoa3.cargo = "vendedora";
+
+		System.out.println(pessoa3.nome);
+		System.out.println(pessoa3.cargo);
+		System.out.println(pessoa3.saldo);
+
+
 	}
 }
