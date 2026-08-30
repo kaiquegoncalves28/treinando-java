a9527e10752b859d4e0680a6eeed32b247c2a6d2
Kaíque Goncalves da Silva <kaiquesilvags728@gmail.com>
2026-08-30 01:39:54 -0300

    ex009

---
+++ exercicios/009 modificador de acesso/src/Empresa/Dados/Funcionario.java
@@ -0,0 +1,11 @@
+package Empresa.Dados;
+public class Funcionario {
+    String nome;
+    double salario;
+    public Funcionario(String nomeInit, double salarioInit) {
+        nome = nomeInit;
+        salario = salarioInit;
+        System.out.println("nome: "+nome);
+        System.out.println("salario: "+salario);
+    }
+}
---
+++ exercicios/009 modificador de acesso/src/Main.java
@@ -0,0 +1,6 @@
+import Empresa.Dados.Funcionario;
+public class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario( "Kaíque", 6896.98);
+    }
+}
